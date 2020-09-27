0aa0796d79c2d3a762ada7b50eac4370f8b21a13
saiteja-code <saitejakanaparthy@gmail.com>
2020-09-27 21:17:10 +0530

    master

---
+++ src/main/java/linecomp/compar/App.java
@@ -0,0 +1,13 @@
+package linecomp.compar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welocome to line comparision computation" );
+    }
+}
